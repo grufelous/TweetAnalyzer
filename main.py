f46ebbc914e3fa757b630dc83570cce8c922c22e
Pratik Anand <grufelous@gmail.com>
2019-08-14 09:26:31 +0530

    Added init file

---
+++ main.py
@@ -0,0 +1,13 @@
+from textblob import TextBlob
+import tweepy
+import matplotlib.pyplot as plt
+import sys
+
+
+def percentage(numer, denom):
+    return 100*float(numer)/float(denom)
+
+consumerKey = ""
+consumerSecret = ""
+accessToken = ""
+accessSecret = ""
